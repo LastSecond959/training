ff6435901bc6aa4053d1142280c6a081af8ce052
LastSecond959 <lastsecond959@gmail.com>
2024-12-11 16:58:04 +0700

    -

--- resources/views/menu/welcome.blade.php
+++ resources/views/menu/welcome.blade.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <!-- <script type="text/javascript">
-            function preventBack() { window.history.forward(); }
-            setTimeout("preventBack()", 0);
-            window.onunload = function () { null };
-        </script> -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Welcome Page</title>
 
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
     </head>
     <body>
         <x-guest-layout>
